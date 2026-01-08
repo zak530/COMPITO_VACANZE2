20a1d980cc13a1975805380a097a0d587d5d1d2c
elhaiki.22132 <elhaiki.zakaria.studente@itispaleocapa.it>
2026-01-08 08:44:52 +0100

    prima funzione

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,31 @@
-public static void main(String[] args) {
+import java.util.Scanner;
 
-    Scanner in = new Scanner(System.in);
-    int opzione = 0;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int opzione;
 
-    do {
-        // stampa menu
-        System.out.println("1 Inizializza CSV (aggiunge miovalore e deleted)");
-        System.out.println("0 Uscita");
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1 - Inizializza CSV (miovalore + deleted)");
+            System.out.println("0 - Esci");
+            System.out.print("Scegli opzione: ");
 
-        // scelta opzione
-        System.out.print("Scegli opzione: ");
-        opzione = Integer.parseInt(in.next());
+            opzione = Integer.parseInt(in.next());
 
-        switch (opzione) {
+            switch (opzione) {
+                case 1:
+                    Csvreader.inizializzaCsv("elhaiki.csv");
+                    break;
+                case 0:
+                    System.out.println("Uscita dal programma");
+                    break;
+                default:
+                    System.out.println("Opzione non valida");
+            }
+        } while (opzione != 0);
 
-            case 1:
-                Csvreader.inizializzaCsv("tuocognome.csv");
-                break;
-
-            case 0:
-                System.out.println("Uscita dal programma");
-                break;
-
-            default:
-                System.out.println("Opzione non valida");
-        }
-
-    } while (opzione != 0);
-
-    System.out.println("Fine programma");
+        in.close();
+        System.out.println("Fine programma");
+    }
 }
